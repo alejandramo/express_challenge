8779da341f67734135a983e968ba4520832293bf
alejandramo <alejandra.momari@gmail.com>
2019-05-16 11:59:44 -0500

    expressc- se agregaron métodos

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ app.all('/hello', (req, res) => res.send('Hello World!'))
 app.all('/bye', (req, res) => res.send('Bye Bye!'))
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
+app.get('/tamal', (req, res) =>{
+    res.status(418).send({sabor:'verde'})
+}
+
+app.post('/tostada', (req, res) =>{
+    res.status(210).send({salsas:'de la que pica'})
+}
+
+app.patch('/tacos', (req, res) =>{
+    res.status(409)
+}
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
